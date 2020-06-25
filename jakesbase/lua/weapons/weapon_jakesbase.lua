8db25f8a1eaf629c7aac26187fbbf42db1ea6ed6
William Smith <wjacobsmith20@gmail.com>
2020-06-25 13:34:43 -0500

    Update weapon_jakesbase.lua

--- jakesbase/lua/weapons/weapon_jakesbase.lua
+++ jakesbase/lua/weapons/weapon_jakesbase.lua
@@ -35,7 +35,7 @@ SWEP.Primary.Sound = Sound("Weapon_AK47.Single")
 SWEP.Primary.Recoil = 1.5
 SWEP.Primary.Damage = 40
 SWEP.Primary.NumShots = 1
-SWEP.Primary.Cone = 0.02
+SWEP.Primary.Cone = 1
 SWEP.Primary.Delay = 0.15
 
 SWEP.Primary.ClipSize = -1
@@ -49,13 +49,72 @@ SWEP.Secondary.Automatic = true
 SWEP.Secondary.Ammo = "none"
 
 -- Placeholder for debugging
-SWEP.IronSightsPos = Vector(15, 0, 0)
-SWEP.IronSightsAng = Vector(0, 0, 0)
+SWEP.IronSightsPos = Vector(-6.6, 0, 2)
+SWEP.IronSightsAng = Vector(2.5, 0, 0)
 
 SWEP.IsAiming = false
 
+function SWEP:SetAimingVM()
+	function self:GetViewModelPosition(eyePos, eyeAng)
+	    local mul = 1.0
+	    local offset = self.IronSightsPos
+
+	    if (self.IronSightsAng) then
+	        eyeAng = eyeAng * 1
+
+	        eyeAng:RotateAroundAxis(eyeAng:Right(), self.IronSightsAng.x * mul)
+	        eyeAng:RotateAroundAxis(eyeAng:Up(), self.IronSightsAng.y * mul)
+	        eyeAng:RotateAroundAxis(eyeAng:Forward(), self.IronSightsAng.z * mul)
+	    end
+
+	    local right = eyeAng:Right()
+	    local up = eyeAng:Up()
+	    local forward = eyeAng:Forward()
+
+	    eyePos = eyePos + offset.x * right * mul
+	    eyePos = eyePos + offset.y * forward * mul
+	    eyePos = eyePos + offset.z * up * mul
+
+	    return eyePos, eyeAng
+	end
+
+	function self:TranslateFOV(fov)
+		return fov - 30
+	end
+end
+
+function SWEP:SetNormalVM()
+	function self:GetViewModelPosition(eyePos, eyeAng)
+	    local mul = 1.0
+
+        eyeAng = eyeAng * 1
+
+        eyeAng:RotateAroundAxis(eyeAng:Right(), 0 * mul)
+        eyeAng:RotateAroundAxis(eyeAng:Up(), 0 * mul)
+        eyeAng:RotateAroundAxis(eyeAng:Forward(), 0 * mul)
+
+	    local right = eyeAng:Right()
+	    local up = eyeAng:Up()
+	    local forward = eyeAng:Forward()
+
+	    eyePos = eyePos + 0 * right * mul
+	    eyePos = eyePos + 0 * forward * mul
+	    eyePos = eyePos + 0 * up * mul
+
+	    return eyePos, eyeAng
+	end
+
+	function self:TranslateFOV(fov)
+		return fov
+	end
+end
+
 function SWEP:Think()
-    self.IsAiming = false
+	self:SetNormalVM()
+end
+
+function SWEP:Initialize()
+	self:SetHoldType(self.HoldType)
 end
 
 function SWEP:PrimaryAttack()
@@ -69,40 +128,12 @@ function SWEP:PrimaryAttack()
 end
 
 function SWEP:SecondaryAttack()
-    if self.IsAiming then return end
-
-    self.IsAiming = true
-end
-
-function SWEP:GetViewModelPosition(eyePos, eyeAng)
-    if not self.IsAiming then return end
-
-    local mul = 1.0
-    local offset = self.IronSightsPos
-
-    if (self.IronSightsAng) then
-        eyeAng = eyeAng * 1
-
-        eyeAng:RotateAroundAxis(eyeAng:Right(), self.IronSightsAng.x * mul)
-        eyeAng:RotateAroundAxis(eyeAng:Up(), self.IronSightsAng.y * mul)
-        eyeAng:RotateAroundAxis(eyeAng:Forward(), self.IronSightsAng.z * mul)
-    end
-
-    local right = eyeAng:Right()
-    local up = eyeAng:Up()
-    local forward = eyeAng:Forward()
-
-    eyePos = eyePos + offset.x * right * mul
-    eyePos = eyePos + offset.y * forward * mul
-    eyePos = eyePos + offset.z * up * mul
-
-    return eyePos, eyeAng
+	self:SetAimingVM()
 end
 
 function SWEP:Reload()
     if not self:DefaultReload(ACT_VM_RELOAD) then return end
 
-    self:SetReloading(true)
+    self:SetNormalVM()
     self:GetOwner():SetAnimation(PLAYER_RELOAD)
-    self:SetReloadEndTime(CurTime() + 2)
 end
