7e94a7cec1ce335008518e83f08d3fa74c256c16
William Smith <wjacobsmith20@gmail.com>
2020-06-25 15:01:19 -0500

    Updated AK47 example
    Updated AK47 example to reduce redundancy with respects to the base, also added variable to work with updated ironsights

--- jakesbase/lua/weapons/weapon_ak47.lua
+++ jakesbase/lua/weapons/weapon_ak47.lua
@@ -1,6 +1,5 @@
 if CLIENT then
-    SWEP.PrintName = "AK47"
-    SWEP.Author = "wjayess"
+    SWEP.Author = "JAKE"
     SWEP.Slot = 3
     SWEP.SlotPos = 0
     SWEP.IconLetter = "b"
@@ -10,31 +9,18 @@ end
 
 SWEP.Base = "weapon_jakesbase"
 
+SWEP.PrintName = "AK47"
+SWEP.Category = "JBase Weapons"
 SWEP.Spawnable = true
-SWEP.AdminOnly = false
 SWEP.SpawnMenuIcon = "vgui/entities/weapon_ak472"
 
-SWEP.UseHands = true
 SWEP.ViewModel = "models/weapons/cstrike/c_rif_ak47.mdl"
 SWEP.WorldModel = "models/weapons/w_rif_ak47.mdl"
 SWEP.HoldType = "ar2"
 
-SWEP.Weight = 5
-SWEP.AutoSwitchTo = true
-SWEP.AutoSwitchFrom = true
-
-SWEP.Primary.Sound = Sound("Weapon_AK47.Single")
-SWEP.Primary.Recoil = 1.5
-SWEP.Primary.Damage = 40
-SWEP.Primary.NumShots = 1
-SWEP.Primary.Cone = 0.002
+SWEP.Primary.RegCone = 0.1
+SWEP.Primary.Cone = 0.1
 SWEP.Primary.ClipSize = 30
 SWEP.Primary.DefaultClip = 30
 SWEP.Primary.Automatic = true
-SWEP.Primary.Delay = 0.13
 SWEP.Primary.Ammo = "smg1"
-
-SWEP.Secondary.ClipSize = -1
-SWEP.Secondary.DefaultClip = -1
-SWEP.Secondary.Automatic = false
-SWEP.Secondary.Ammo = "none"
